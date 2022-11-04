7f9eabb1e97644edab49396683b2f1e316dcc625
patrick <mukumbu.katiwa@boldlink.io>
2022-11-04 07:38:38 +0300

    Add public subnets for NAT

--- tests/supportingResources/locals.tf
+++ tests/supportingResources/locals.tf
@@ -1,5 +1,6 @@
 locals {
   private_subnets = [cidrsubnet(local.cidr_block, 8, 1), cidrsubnet(local.cidr_block, 8, 2), cidrsubnet(local.cidr_block, 8, 3)]
+  public_subnets  = [cidrsubnet(local.cidr_block, 8, 4), cidrsubnet(local.cidr_block, 8, 5), cidrsubnet(local.cidr_block, 8, 6)]
   region          = data.aws_region.current.id
   account_id      = data.aws_caller_identity.current.id
   azs             = flatten(data.aws_availability_zones.available.names)
--- tests/supportingResources/main.tf
+++ tests/supportingResources/main.tf
@@ -1,15 +1,17 @@
 module "ecs_vpc" {
-  source             = "boldlink/vpc/aws"
-  version            = "2.0.3"
-  name               = local.name
-  account            = local.account_id
-  region             = local.region
-  cidr_block         = local.cidr_block
-  create_nat_gateway = true
-  nat_single_az      = true
-  private_subnets    = local.private_subnets
-  availability_zones = local.azs
-  tag_env            = local.tag_env
+  source               = "boldlink/vpc/aws"
+  version              = "2.0.3"
+  name                 = local.name
+  account              = local.account_id
+  region               = local.region
+  cidr_block           = local.cidr_block
+  create_nat_gateway   = true
+  nat_single_az        = true
+  private_subnets      = local.private_subnets
+  public_subnets       = local.public_subnets
+  enable_dns_hostnames = true
+  availability_zones   = local.azs
+  tag_env              = local.tag_env
 
   other_tags = {
     "user::CostCenter" = "terraform"
