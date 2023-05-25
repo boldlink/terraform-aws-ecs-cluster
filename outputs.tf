af7e47fc65d27f3208dbddcd04c923b29f2487e3
john <jmwangi288@gmail.com>
2023-05-25 20:06:46 +0300

    fix: kms key outputs

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,16 @@ output "arn" {
   value       = aws_ecs_cluster.this.arn
 }
 
+output "id" {
+  description = "ARN that identifies the cluster."
+  value       = aws_ecs_cluster.this.id
+}
+
+output "tags_all" {
+  description = "Map of tags assigned to the resource, including those inherited from the provider default_tags"
+  value       = aws_ecs_cluster.this.tags_all
+}
+
 output "key_arn" {
   description = "The Amazon Resource Name (ARN) of the key."
   value       = aws_kms_key.main.*.arn
